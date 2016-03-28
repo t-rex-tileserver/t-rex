247e05b0a3c7c87ef683b32fac9a6eeccb9bc4e8
Pirmin Kalberer <pka@sourcepole.ch>
2016-03-28 22:40:55 +0200

    MultiPoint and Linestring encoding

--- src/mvt/geom_to_proto.rs
+++ src/mvt/geom_to_proto.rs
@@ -44,13 +44,11 @@ pub struct ParameterInteger(u32);
 
 impl ParameterInteger {
     pub fn new(value: i32) -> ParameterInteger {
-        let uval = value as u32; 
-        ParameterInteger((uval << 1) ^ (uval >> 31))
+        ParameterInteger(((value << 1) ^ (value >> 31)) as u32)
     }
 
     pub fn value(&self) -> i32 {
-        let sval = self.0 as i32; 
-        ((sval >> 1) ^ (-(sval & 1)))
+        ((self.0 >> 1) as i32) ^ (-((self.0 & 1) as i32))
     }
 }
 
@@ -63,6 +61,17 @@ fn test_paremeters() {
 
 pub struct CommandSequence(Vec<u32>);
 
+impl CommandSequence {
+    pub fn new() -> CommandSequence {
+        CommandSequence(Vec::new())
+    }
+    pub fn append(&mut self, other: &mut CommandSequence) {
+        self.0.append(&mut other.0);
+    }
+    pub fn push(&mut self, value: u32) {
+        self.0.push(value);
+    }
+}
 
 // Geometry types in screen coordinates
 
@@ -81,8 +90,67 @@ impl PointScreen {
     }
 }
 
+struct MultiPointScreen {
+    points: Vec<PointScreen>
+}
+
+impl MultiPointScreen {
+    pub fn encode(&self) -> CommandSequence {
+        let mut seq = CommandSequence::new();
+        seq.push(CommandInteger::new(
+            Command::MoveTo, self.points.len() as u32).0);
+        let (mut posx, mut posy) = (0, 0);
+        for point in &self.points {
+            seq.push(ParameterInteger::new(point.x - posx).0);
+            seq.push(ParameterInteger::new(point.y - posy).0);
+            posx = point.x;
+            posy = point.y;
+        }
+        seq
+    }
+}
+
+struct LinestringScreen {
+    points: Vec<PointScreen>
+}
+
+impl LinestringScreen {
+    pub fn encode(&self) -> CommandSequence {
+        let mut seq = CommandSequence::new();
+        if self.points.len() > 0 {
+            seq = self.points[0].encode();
+            seq.push(CommandInteger::new(
+                Command::LineTo, (self.points.len()-1) as u32).0);
+            for i in 1..self.points.len() {
+                let ref pos = &self.points[i-1];
+                let ref point = &self.points[i];
+                seq.push(ParameterInteger::new(point.x - pos.x).0);
+                seq.push(ParameterInteger::new(point.y - pos.y).0);
+            }
+        }
+        seq
+    }
+}
+
 #[test]
 fn test_geom_encoding() {
     let point = PointScreen { x: 25, y: 17 };
     assert_eq!(point.encode().0, &[9,50,34]);
+
+    let multipoint = MultiPointScreen {
+        points: vec![
+            PointScreen { x: 5, y: 7 },
+            PointScreen { x: 3, y: 2 }
+            ]
+        };
+    assert_eq!(multipoint.encode().0, &[17,10,14,3,9]);
+
+    let linestring = LinestringScreen {
+        points: vec![
+            PointScreen { x: 2, y: 2 },
+            PointScreen { x: 2, y: 10 },
+            PointScreen { x: 10, y: 10 }
+            ]
+        };
+    assert_eq!(linestring.encode().0, &[9,4,4,18,0,16,16,0]);
 }
