046a2b6f8f4a4518d0417f331454f77516d19e7a
Pirmin Kalberer <pka@sourcepole.ch>
2016-05-01 22:49:26 +0200

    select_geometries test code

--- src/datasource/postgis.rs
+++ src/datasource/postgis.rs
@@ -18,9 +18,27 @@ impl PostgisInput {
         let table_name: String = rows.get(0).get("f_table_name");
         table_name
     }
+    pub fn select_geometries(&self) {
+        let conn = Connection::connect(self.connection_url, SslMode::None).unwrap();
+        let stmt = conn.prepare("SELECT geometry FROM osm_place_point LIMIT 2").unwrap();
+        for row in &stmt.query(&[]).unwrap() {
+            println!(">>>>>> {}", row.get::<_, geom::Point<EPSG_3857>>("geometry"));
+        }
+        let stmt = conn.prepare("SELECT geometry FROM osm_water_linestring LIMIT 2").unwrap();
+        for row in &stmt.query(&[]).unwrap() {
+            println!(">>>>>> {}", row.get::<_, geom::LineString<geom::Point<EPSG_3857>>>("geometry"));
+        }
+    }
     pub fn get_features(&self, layer: &str, extent: &Extent) -> geom::Point<EPSG_3857> {
         geom::Point::<EPSG_3857>::new(960000.0, 6002729.0)
     }
+}
+
+#[cfg(feature = "dbtest")]
+#[test]
+pub fn test_geometry_types() {
+    let pg = PostgisInput {connection_url: "postgresql://pi@%2Frun%2Fpostgresql/osm2vectortiles"};
+    pg.select_geometries();
 }
 
 #[cfg(feature = "dbtest")]
