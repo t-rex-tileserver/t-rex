dfb39502cf61593a45b4a3c12a898219ae243046
Pirmin Kalberer <pka@sourcepole.ch>
2016-05-09 20:24:14 +0200

    Nickel http hello world

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,16 @@
 extern crate t_rex;
+#[macro_use] extern crate nickel;
+
+use nickel::Nickel;
 
 fn main() {
-    println!("Hello, world!");
+    let mut server = Nickel::new();
+
+    server.utilize(router! {
+        get "**" => |_req, _res| {
+            "Hello world!"
+        }
+    });
+
+    server.listen("127.0.0.1:6767");
 }
