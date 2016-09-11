d491a48f315020cd499d849b8276bb8754ca7838
Pirmin Kalberer <pka@sourcepole.ch>
2016-09-11 22:43:57 +0200

    Version bump

--- src/main.rs
+++ src/main.rs
@@ -86,7 +86,7 @@ fn main() {
 
     // http://kbknapp.github.io/clap-rs/clap/
     let mut app = App::new("t_rex")
-                        .version("0.3.1")
+                        .version("0.4.0")
                         .author("Pirmin Kalberer <pka@sourcepole.ch>")
                         .about("vector tile server specialized on publishing MVT tiles from a PostGIS database")
                         .subcommand(SubCommand::with_name("serve")
