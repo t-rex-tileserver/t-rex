85ae6a35017ebcf0907ad6cbf72a101dd35d8c7b
Pirmin Kalberer <pka@sourcepole.ch>
2016-12-06 11:04:22 +0100

    CI: PostGIS setup on OSX

--- ci/install.sh
+++ ci/install.sh
@@ -52,19 +52,18 @@ configure_postgis() {
     if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
         # http://stackoverflow.com/questions/36875239/travis-os-x-test-postgres/36945462#36945462
         export PG_DATA=$(brew --prefix)/var/postgres
-        pg_ctl -w start -l postgres.log --pgdata ${PG_DATA} || cat postgres.log
         #FATAL:  database files are incompatible with server
         #DETAIL:  The data directory was initialized by PostgreSQL version 9.4, which is not compatible with this version 9.5.4.
-        #createuser -s postgres
-        #cat postgres.log
+        rm -rf $PG_DATA
+        initdb $PG_DATA -E utf8
+        pg_ctl -w start -l postgres.log --pgdata ${PG_DATA}
+        createuser -s postgres
     fi
-    if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-        export PGUSER=postgres
-        #cd src/test
-        #make
-        wget http://pkg.sourcepole.ch/ne_t_rex_test.dump
-        pg_restore --create --no-owner -d postgres ne_t_rex_test.dump
-    fi
+    export PGUSER=postgres
+    #cd src/test
+    #make
+    wget http://pkg.sourcepole.ch/ne_t_rex_test.dump
+    pg_restore --create --no-owner -d postgres ne_t_rex_test.dump
 }
 
 main() {
