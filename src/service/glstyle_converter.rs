7933a25a94a6c41453745c6eb83d56adebac4a93
Pirmin Kalberer <pka@sourcepole.ch>
2016-12-19 23:10:18 +0100

    Zoom function conversion

--- src/service/glstyle_converter.rs
+++ src/service/glstyle_converter.rs
@@ -3,14 +3,77 @@
 // Licensed under the MIT License. See LICENSE file in the project root for full license information.
 //
 
+use std::collections::BTreeMap;
+use rustc_serialize::json::{ToJson, Json};
 use toml;
-use rustc_serialize::json::{self, Json};
+use std;
+
+use toml::Value::{self, String, Integer, Float, Boolean, Datetime, Array, Table};
 
 
 /// Convert Mapbox GL Styles from [TOML format](https://pka.github.io/mapbox-gl-style-spec/) to JSON
-pub fn toml_style_to_gljson(toml: &toml::Value) -> String {
-    let configjson = json::encode(toml).unwrap().replace("}{", "},{").replace("][", "],[");
-    Json::from_str(&configjson).unwrap().pretty().to_string()
+fn toml_style_to_gljson(toml: &toml::Value) -> std::string::String {
+    let converter = TomlConverter::new();
+    let json = converter.convert_value(toml);
+    json.pretty().to_string()
+}
+
+
+struct TomlConverter;
+impl TomlConverter {
+    pub fn new() -> TomlConverter {
+        TomlConverter
+    }
+
+    pub fn convert_value(&self, toml: &toml::Value) -> Json {
+        match *toml {
+            Table(ref value) => self.convert_table(value),
+
+            Array(ref array) => {
+                let mut vec = Vec::new();
+                for value in array.iter() {
+                    vec.push(self.convert_value(value));
+                }
+                vec.to_json()
+            }
+
+            String(ref value) => value.to_json(),
+            Integer(ref value) => value.to_json(),
+            Float(ref value) => value.to_json(),
+            Boolean(ref value) => value.to_json(),
+            Datetime(ref value) => value.to_json(),
+        }
+    }
+
+    pub fn convert_table(&self, table: &BTreeMap<std::string::String, Value>) -> Json {
+        let mut json: BTreeMap<std::string::String, Json> = BTreeMap::new();
+        for (key, value) in table.iter() {
+            if key == "stops" {
+                let mut stops = Vec::new();
+                if let Array(ref stops_arr) = *value {
+                    for stop in stops_arr.iter() {
+                        if let Table(ref stops_tbl) = *stop {
+                            if stops_tbl.contains_key("in") {
+                                if stops_tbl.contains_key("out") {
+                                    stops.push(vec![
+                                        self.convert_value(&stops_tbl["in"]),
+                                        self.convert_value(&stops_tbl["out"])]);
+                                } else {
+                                    stops.push(vec![
+                                        self.convert_value(&stops_tbl["in"])]);
+                                }
+                            }
+                        }
+                    }
+
+                }
+                json.insert(key.to_string(), stops.to_json());
+            } else {
+                json.insert(key.to_string(), self.convert_value(value));
+            }
+        }
+        json.to_json()
+    }
 }
 
 
@@ -32,14 +95,14 @@ pub fn color_stops() {
   "circle-color": {
     "property": "temperature",
     "stops": [
-      {
-        "in": 0,
-        "out": "blue"
-      },
-      {
-        "in": 100,
-        "out": "red"
-      }
+      [
+        0,
+        "blue"
+      ],
+      [
+        100,
+        "red"
+      ]
     ]
   }
 }"##;
@@ -56,7 +119,7 @@ pub fn color_stops() {
 
         [layers.paint.fill-translate]
         base = 1
-        stops = [{ in = 15 }, { in = 16 }]"##;
+        stops = [{ in = 15, out = [0,0] }, { in = 16, out = [-2,-2] }]"##;
 
     let mut parser = Parser::new(style);
     let toml = Value::Table(parser.parse().unwrap());
@@ -70,26 +133,34 @@ pub fn color_stops() {
       "fill-opacity": {
         "base": 1,
         "stops": [
-          {
-            "in": 15,
-            "out": 0
-          },
-          {
-            "in": 16,
-            "out": 1
-          }
+          [
+            15,
+            0
+          ],
+          [
+            16,
+            1
+          ]
         ]
       },
       "fill-outline-color": "#dfdbd7",
       "fill-translate": {
         "base": 1,
         "stops": [
-          {
-            "in": 15
-          },
-          {
-            "in": 16
-          }
+          [
+            15,
+            [
+              0,
+              0
+            ]
+          ],
+          [
+            16,
+            [
+              -2,
+              -2
+            ]
+          ]
         ]
       }
     }
@@ -98,6 +169,90 @@ pub fn color_stops() {
     assert_eq!(configjson, expected);
 }
 
+#[test]
+pub fn filters() {
+    use toml::{Value, Parser};
+
+    let style = r##"
+        [[layers]]
+        id = "tunnel_path_pedestrian"
+        type = "line"
+        source = "mapbox"
+        source-layer = "road"
+        filter = [["all"], ["==", "$type", "LineString"], [["all"], ["==", "structure", "tunnel"], ["in", "class", "path", "pedestrian"]]]
+        interactive = true"##;
+
+    let mut parser = Parser::new(style);
+    let toml = Value::Table(parser.parse().unwrap());
+
+    let configjson = toml_style_to_gljson(&toml);
+    println!("{}", configjson);
+    let expected= r##"{
+  "layers": [
+    {
+      "filter": [
+        [
+          "all"
+        ],
+        [
+          "==",
+          "$type",
+          "LineString"
+        ],
+        [
+          [
+            "all"
+          ],
+          [
+            "==",
+            "structure",
+            "tunnel"
+          ],
+          [
+            "in",
+            "class",
+            "path",
+            "pedestrian"
+          ]
+        ]
+      ],
+      "id": "tunnel_path_pedestrian",
+      "interactive": true,
+      "source": "mapbox",
+      "source-layer": "road",
+      "type": "line"
+    }
+  ]
+}"##;
+    /* TODO: Should be
+    "id": "tunnel_path_pedestrian",
+    "type": "line",
+    "source": "mapbox",
+    "source-layer": "road",
+    "filter": [
+        "all",
+        [
+            "==",
+            "$type",
+            "LineString"
+        ],
+        [
+            "all",
+            [
+                "==",
+                "structure",
+                "tunnel"
+            ],
+            [
+                "in",
+                "class",
+                "path",
+                "pedestrian"
+            ]
+        ]
+    ],*/
+    assert_eq!(configjson, expected);
+}
 
 #[test]
 pub fn layer_style_from_cfg() {
@@ -127,14 +282,14 @@ pub fn layer_style_from_cfg() {
     "fill-color": {
       "base": 1,
       "stops": [
-        {
-          "in": 15.5,
-          "out": "#f2eae2"
-        },
-        {
-          "in": 16,
-          "out": "#dfdbd7"
-        }
+        [
+          15.5,
+          "#f2eae2"
+        ],
+        [
+          16,
+          "#dfdbd7"
+        ]
       ]
     }
   },
