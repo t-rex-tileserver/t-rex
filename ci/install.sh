c5b3d680f5edcbcbb309daa10464daca226cfc2c
Pirmin Kalberer <pka@sourcepole.ch>
2017-03-29 00:14:46 +0200

    CI templates from trust v0.1.1

--- ci/install.sh
+++ ci/install.sh
@@ -1,77 +1,31 @@
-# `install` phase: install stuff needed for the `script` phase
-
 set -ex
 
-. $(dirname $0)/utils.sh
-
-install_c_toolchain() {
-    case $TARGET in
-        aarch64-unknown-linux-gnu)
-            sudo apt-get install -y --no-install-recommends \
-                 gcc-aarch64-linux-gnu libc6-arm64-cross libc6-dev-arm64-cross
-            ;;
-        *)
-            # For other targets, this is handled by addons.apt.packages in .travis.yml
-            ;;
-    esac
-}
-
-install_rustup() {
-    # uninstall the rust toolchain installed by travis, we are going to use rustup
-    sh ~/rust/lib/rustlib/uninstall.sh
-
-    curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain=$TRAVIS_RUST_VERSION
-
-    rustc -V
-    cargo -V
-}
-
-install_standard_crates() {
-    if [ $(host) != "$TARGET" ]; then
-        rustup target add $TARGET
-    fi
-}
-
-configure_cargo() {
-    local prefix=$(gcc_prefix)
-
-    if [ ! -z $prefix ]; then
-        # information about the cross compiler
-        ${prefix}gcc -v
-
-        # tell cargo which linker to use for cross compilation
-        mkdir -p .cargo
-        cat >>.cargo/config <<EOF
-[target.$TARGET]
-linker = "${prefix}gcc"
-EOF
-    fi
-}
-
-configure_postgis() {
-    if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-        # http://stackoverflow.com/questions/36875239/travis-os-x-test-postgres/36945462#36945462
-        export PG_DATA=$(brew --prefix)/var/postgres
-        #FATAL:  database files are incompatible with server
-        #DETAIL:  The data directory was initialized by PostgreSQL version 9.4, which is not compatible with this version 9.5.4.
-        rm -rf $PG_DATA
-        initdb $PG_DATA -E utf8
-        pg_ctl -w start -l postgres.log --pgdata ${PG_DATA}
-        createuser -s postgres
-    fi
-    export PGUSER=postgres
-    #cd src/test
-    #make
-    wget http://pkg.sourcepole.ch/ne_t_rex_test.dump
-    pg_restore --create --no-owner -d postgres ne_t_rex_test.dump
-}
-
 main() {
-    install_c_toolchain
-    install_rustup
-    install_standard_crates
-    configure_cargo
-    configure_postgis
+    curl https://sh.rustup.rs -sSf | \
+        sh -s -- -y --default-toolchain $TRAVIS_RUST_VERSION
+
+    local target=
+    if [ $TRAVIS_OS_NAME = linux ]; then
+        target=x86_64-unknown-linux-gnu
+        sort=sort
+    else
+        target=x86_64-apple-darwin
+        sort=gsort  # for `sort --sort-version`, from brew's coreutils.
+    fi
+
+    # This fetches latest stable release
+    local tag=$(git ls-remote --tags --refs --exit-code https://github.com/japaric/cross \
+                       | cut -d/ -f3 \
+                       | grep -E '^v[0-9.]+$' \
+                       | $sort --version-sort \
+                       | tail -n1)
+    echo cross version: $tag
+    curl -LSfs https://japaric.github.io/trust/install.sh | \
+        sh -s -- \
+           --force \
+           --git japaric/cross \
+           --tag $tag \
+           --target $target
 }
 
 main
--- ci/script.sh
+++ ci/script.sh
@@ -1,55 +1,24 @@
-# `script` phase: you usually build, test and generate docs in this phase
+# This script takes care of testing your crate
 
 set -ex
 
-. $(dirname $0)/utils.sh
-
-# NOTE Workaround for rust-lang/rust#31907 - disable doc tests when cross compiling
-# This has been fixed in the nightly channel but it would take a while to reach the other channels
-disable_cross_doctests() {
-    if [ $(host) != "$TARGET" ] && [ "$TRAVIS_RUST_VERSION" = "stable" ]; then
-        if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-            brew install gnu-sed --default-names
-        fi
-
-        find src -name '*.rs' -type f | xargs sed -i -e 's:\(//.\s*```\):\1 ignore,:g'
-    fi
-}
-
-# TODO modify this function as you see fit
-# PROTIP Always pass `--target $TARGET` to cargo commands, this makes cargo output build artifacts
-# to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
-# `before_deploy`/packaging phase
-run_test_suite() {
-    case $TARGET in
-        # configure emulation for transparent execution of foreign binaries
-        aarch64-unknown-linux-gnu)
-            export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
-            ;;
-        arm*-unknown-linux-gnueabihf)
-            export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
-            ;;
-        *)
-            ;;
-    esac
-
-    if [ ! -z "$QEMU_LD_PREFIX" ]; then
-        # Run tests on a single thread when using QEMU user emulation
-        export RUST_TEST_THREADS=1
-    fi
-
-    cargo build --target $TARGET --verbose
-    cargo run --target $TARGET
-    cargo test --target $TARGET
-    cargo test --target $TARGET -- --ignored
-
-    # sanity check the file type
-    file target/$TARGET/debug/t_rex
-}
-
+# TODO This is the "test phase", tweak it as you see fit
 main() {
-    disable_cross_doctests
-    run_test_suite
+    cross build --target $TARGET
+    cross build --target $TARGET --release
+
+    if [ ! -z $DISABLE_TESTS ]; then
+        return
+    fi
+
+    cross test --target $TARGET
+    cross test --target $TARGET --release
+
+    cross run --target $TARGET
+    cross run --target $TARGET --release
 }
 
-main
+# we don't run the "test phase" when doing deploys
+if [ -z $TRAVIS_TAG ]; then
+    main
+fi
