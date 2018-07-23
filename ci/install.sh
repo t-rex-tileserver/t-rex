616516a6cabfa4d9051a9aa81aafb51eab2c2ec0
Pirmin Kalberer <pka@sourcepole.ch>
2018-07-24 00:19:09 +0200

    Skip PG tests on Travis MacOS

--- ci/install.sh
+++ ci/install.sh
@@ -1,20 +1,6 @@
 set -ex
 
 configure_postgis() {
-    if [ $TRAVIS_OS_NAME = osx ]; then
-        # http://stackoverflow.com/questions/36875239/travis-os-x-test-postgres/36945462#36945462
-        export PG_DATA=$(brew --prefix)/var/postgres
-        #FATAL:  database files are incompatible with server
-        #DETAIL:  The data directory was initialized by PostgreSQL version 9.4, which is not compatible with this version 9.5.4.
-        rm -rf $PG_DATA
-        initdb $PG_DATA -E utf8
-        pg_ctl -w start -l postgres.log --pgdata ${PG_DATA}
-        createuser -s postgres
-
-        wget http://pkg.sourcepole.ch/ne_t_rex_test.dump
-        export PGUSER=postgres
-        pg_restore --create --no-owner -d postgres ne_t_rex_test.dump
-    fi
     if [ $TRAVIS_OS_NAME = linux ]; then
         export PGUSER=postgres
         cd t-rex-service/src/test
